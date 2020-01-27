090919cbcc6110b1e385a6e3e614cb4c110f5d18
razvanniculescu24 <60292096+razvanniculescu24@users.noreply.github.com>
2020-01-27 19:00:35 +0200

    add exception when there is only birth date

--- selenium/src/test/java/org/home/FirstTest.java
+++ selenium/src/test/java/org/home/FirstTest.java
@@ -6,14 +6,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.*;
+import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FirstTest
 {
     @Test
-    public void test1() throws IOException {
+    public void test1() throws IOException, InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         String wikiUrl = "https://en.wikipedia.org";
@@ -35,12 +39,12 @@ public class FirstTest
 
             ClearInputField(inputField);
 
-            inputField.sendKeys(nameToSearch);
+            InputPersonalityString(inputField, nameToSearch);
+
             searchButton.click();
 
-            String allString = GetString(driver);
+            String dates = GetDates(GetAllString(driver, nameToSearch));
 
-            String dates = GetDates(allString);
             System.out.println(currentLine + dates);
         }
         System.out.println("***************************************************\n");
@@ -48,24 +52,46 @@ public class FirstTest
         driver.quit();
 }
         private static void ClearInputField(WebElement inputField){
+            inputField.click();
             inputField.sendKeys(Keys.CONTROL + "a");
             inputField.sendKeys(Keys.BACK_SPACE);
         }
 
-        private static String GetString(WebDriver driver){
-            WebElement dates = driver.findElement(By.xpath("//table[starts-with(@class, 'infobox')]/following-sibling::p[1]"));
-            return dates.getText();
+        private static String GetAllString(WebDriver driver, String nameToSearch) {
+         try {
+             String locator = "//div[@id= 'content' and contains(., '"+nameToSearch+"')]//div[@class='mw-parser-output']/p[descendant::b][1]";
+             WebElement allString = (new WebDriverWait(driver, 4))
+                     .until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+             return allString.getText();
+         }
+         catch (NoSuchElementException e) {
+             return "Element not found. ";
+         }
         }
 
         private static String GetDates(String inputString){
-            String pattern = "\\d{1,2} (January|February|March|April|May|June|July|August|September|October|November|December) \\d{4} " +
+            String pattern1 = "\\d{1,2} (January|February|March|April|May|June|July|August|September|October|November|December) \\d{4} " +
                     "(\\-|\\–) \\d{1,2} (January|February|March|April|May|June|July|August|September|October|November|December) \\d{4}" +
                     "|(January|February|March|April|May|June|July|August|September|October|November|December) \\d{1,2}, \\d{4} " +
                     "(\\-|\\–) (January|February|March|April|May|June|July|August|September|October|November|December) \\d{1,2}, \\d{4}";
-            Pattern r = Pattern.compile(pattern);
-            Matcher m = r.matcher(inputString);
-            if(m.find()) return ": " + m.group(0).replace("–", "-");
-            else return ": Could not found the entire date format.";
+            String pattern2 = "\\d{1,2} (January|February|March|April|May|June|July|August|September|October|November|December) \\d{4}" +
+                    "|(January|February|March|April|May|June|July|August|September|October|November|December) \\d{1,2}, \\d{4}";
+            Pattern r1 = Pattern.compile(pattern1);
+            Matcher m1 = r1.matcher(inputString);
+            Pattern r2 = Pattern.compile(pattern2);
+            Matcher m2 = r2.matcher(inputString);
+            if(m1.find()) return ": " + m1.group().replace("–", "-");
+            if(m2.find()) return ": " + m2.group();
+            else return ": Could not find the proper date. ";
+        }
+
+        private static void InputPersonalityString(WebElement inputField, String nameToSearch) throws InterruptedException {
+            inputField.sendKeys(nameToSearch);
+            if(inputField.getCssValue("value") != nameToSearch){
+                Thread.sleep(300);  //manual sleep because it seems that after some searches the web page auto refreshes
+                ClearInputField(inputField);
+                inputField.sendKeys(nameToSearch);
+            }
         }
 }
 
